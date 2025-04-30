d5ad7b98102f2590b6abcd640d0e65bd862558b1
Егор Крылов <150360940+MandomWrath@users.noreply.github.com>
2025-04-30 21:23:51 +0300

    Update 1.1.py

--- 1.1.py
+++ 1.1.py
@@ -41,4 +41,25 @@ class Solution:
         return self.sortArray(a) +c + self.sortArray(b)
 
 \\\\\\\\\\\
+#heap
+class Solution:
+    def HeapArray(self,nums,n,i):
+        largest = i
+        left = 2*i+1
+        right = 2*i+2
+        if left < n and nums[left] >nums[largest]:
+            largest = left
+        if right < n and nums[right] >nums[largest]:
+            largest = right
+        if largest !=i:
+            nums[i], nums[largest] = nums[largest], nums[i]
+            self.HeapArray(nums,n,largest)
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        for i in range(n//2-1,-1,-1):
+            self.HeapArray(nums,n,i)
+        for i in range(n-1,0,-1):
+            nums[0], nums[i] = nums[i], nums[0]
+            self.HeapArray(nums,i,0)
+        return nums
 
